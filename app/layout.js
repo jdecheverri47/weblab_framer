9da9f230d1ff0a8050c347f62d504fd7712ecb80
jdecheverri47 <josecheverri47@gmail.com>
2023-11-16 09:36:41 -0500

    Add Konnect fonts and update body class in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className=''>
-      <body>
+      <body className=''>
         <Header />
         {children}
       </body>
